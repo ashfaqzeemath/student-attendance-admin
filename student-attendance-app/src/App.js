46a69524f9e541faac08849263c7c965376d2d58
Chathra Serasinghe <chathraserasinghe89@gmail.com>
2021-02-28 00:19:28 +0530

    Students Attendance Web App initial commit with Pipeline

---
+++ student-attendance-app/src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import logo2 from './Student_man.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo2} className="App-logo" alt="logo" />
+        <p>ALSET Student Attendance Admin Panel</p>
+        {/* <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p> */}
+        {/* <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a> */}
+      </header>
+    </div>
+  );
+}
+
+export default App;
