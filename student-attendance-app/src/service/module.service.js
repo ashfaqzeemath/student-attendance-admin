b898c4e95406fefb173ecbb621bab7ea81df6506
ashfaqzeemath <ashfaqzeemath@gmail.com>
2021-04-14 18:16:08 +0530

    added module.service.js file

---
+++ student-attendance-app/src/service/module.service.js
@@ -0,0 +1,12 @@
+export const postModule = async (body) => {
+    const response = await fetch("https://d6otawvpj7.execute-api.us-east-1.amazonaws.com/dev/add-module", {
+      method: "POST",
+      headers: {
+        // Accept: "/",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    });
+  
+    return response;
+  };
