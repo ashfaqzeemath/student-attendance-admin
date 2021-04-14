d0401523dc6dd680700c9f11ce5350dc2b7b3055
ashfaqzeemath <ashfaqzeemath@gmail.com>
2021-04-14 18:13:36 +0530

    call Add module api

--- student-attendance-app/src/pages/EnterModulePage.js
+++ student-attendance-app/src/pages/EnterModulePage.js
@@ -1,5 +1,5 @@
 import Page from 'components/Page';
-import React from 'react';
+import React, {useState} from 'react';
 import {
   Button,
   Card,
@@ -14,8 +14,46 @@ import {
   Label,
   Row,
 } from 'reactstrap';
+import { postModule } from '../service/module.service';
+
 
 const EnterModulePage = () => {
+  const [moduleId, setModuleId] = useState("")
+  const [name, setName] = useState("")
+  const [lecturerId, setLecturerId] = useState("")
+  const [courseId, setCourseId] = useState("")
+  
+const InitModuleIdError = {errorMessage: "",
+                          overall: false,
+                          required: false,
+                          inavlid: false};
+
+  const [moduleIdError, setmoduleIdError] = useState(InitModuleIdError);
+
+  const OnSubmit = () => {
+    if (moduleIdError.overall) {
+      return;
+    }
+
+    const body = {"module_id":moduleId,"name":name,"lecturer_id":lecturerId,"course_id":courseId};
+    postModule(body)
+    .then(
+      res => res.json()
+    )
+    .then(json => {
+      if (json && json.status === "successful" ) {
+        //call was sucssess.
+        setModuleId("");
+        setName("");
+        setmoduleIdError(InitModuleIdError);
+      }
+    })
+    .catch(err => {
+      console.log(err);
+    })
+  }
+
+  //{"module_id":"3","name":"SNA","lecturer_id":"1","course_id":"1"}
   return (
     <Page title="Enter Module" breadcrumbs={[{ name: 'Enter Module', active: true }]}>
       <Row>
@@ -28,23 +66,46 @@ const EnterModulePage = () => {
                     <Label for="module_id">Module ID</Label>
                     <Input 
                         type="text"
-                        name="module_id"/>
+                        name="module_id"
+                        value={moduleId}
+                        onChange={(event) => setModuleId(event.target.value)}
+                        onBlur={(event) => {
+                          if (event.target.value.trim() === "") {
+                            setmoduleIdError({errorMessage: "This field is required",
+                                            overall: true,
+                                            required: true,
+                                            inavlid: false})
+                          }
+                        }} />
+                    {moduleIdError.overall && <span>{moduleIdError.errorMessage}</span>}
                 </FormGroup>
                 <FormGroup>
-                    <Label for="name">Module Name</Label>
+                    <Label for="name">Name</Label>
                     <Input 
                         type="text"
-                        name="name"/>
+                        name="name"
+                        value={name}
+                        onChange={(event) => setName(event.target.value)} />
                 </FormGroup>
                 <FormGroup>
-                <Label for="dept_id">Department</Label>
-                    <Input
-                    type="text"
-                    name="dept_id" />
+                    <Label for="lecturer_id">Lecturer</Label>
+                    <Input 
+                        type="text"
+                        name="lecturer_id"
+                        value={lecturerId}
+                        onChange={(event) => setLecturerId(event.target.value)} />
+                </FormGroup>
+                <FormGroup>
+                    <Label for="course_id">Course Id</Label>
+                    <Input 
+                        type="text"
+                        name="course_id"
+                        value={courseId}
+                        onChange={(event) => setCourseId(event.target.value)} />
                 </FormGroup>
                 <FormGroup check row>
                   <Col sm={{ size: 10, offset: 2 }}>
-                    <Button>Submit</Button>
+                    <Button onClick={OnSubmit}>Submit</Button>
                   </Col>
                 </FormGroup>
               </Form>
